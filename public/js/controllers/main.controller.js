f7b1dbd7e66099c92238b36d30fd75580e8e7b9a
johned <johned.matt@gmail.com>
2016-10-24 20:19:52 -0400

    implemetned todo service and cleaned up previous code

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -2,17 +2,15 @@
   angular.module('intro') //getter syntax
   .controller('MainController', MainController);
 
-  MainController.$inject = ['$scope']; //what tools the main controller function needs
+  MainController.$inject = ['$scope', 'TodoService']; //what tools the main controller function needs
 
-  function MainController($scope){ //scope is our bridge to the DOM
-    console.log("Now main controlling...");
-    $scope.person = 'Bob';
-    $scope.letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    $scope.saySurprise = saySurprise;
-
-    function saySurprise(person){
-      // $scope.person = 1; //don't do this
-      alert('Hey '+ person + '... there is a clown behind you!');
-    }
+  function MainController($scope, TodoService){ //scope is our bridge to the DOM
+    console.log(TodoService.get());
+    TodoService.create('make more todos');
+    console.log(TodoService.get());
+    TodoService.update(0, 'buy some ramen');
+    console.log(TodoService.get());
+    TodoService.delete(2);
+    console.log(TodoService.get());
   }
 }());
